a4827ff2af9424fcee8d897bea950d527d57dee0
clickadelic <toby.hopp@gmail.com>
2024-03-16 00:49:16 +0100

    custom variant

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
 	return (
-		<Button size="lg" variant="ghost">
+		<Button size="sm" variant="custom">
 			Click me
 		</Button>
 	)
