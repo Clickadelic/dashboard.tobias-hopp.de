03eafe5a7d6d08524f120bd4ef786e0cf39eaaa9
clickadelic <toby.hopp@gmail.com>
2024-03-16 00:39:21 +0100

    shadcn install, button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<p>Dashboard Content</p>
-		</main>
-	);
+		<Button size="lg" variant="ghost">
+			Click me
+		</Button>
+	)
 }
