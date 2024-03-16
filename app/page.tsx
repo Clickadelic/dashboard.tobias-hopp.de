23298e9a1d143af1a8388ea19b259fb512bac4dc
clickadelic <toby.hopp@gmail.com>
2024-03-16 01:34:12 +0100

    Start screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
 	return (
-		<Button size="sm" variant="custom">
-			Click me
-		</Button>
+		<main className="flex flex-col h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-900">
+			<div className="space-y-6">
+				<h1 className="text-6xl font-semibold text-white shadow-sm">🔒Auth</h1>
+				<p className="text-white">A simple authentication service.</p>
+			</div>
+		</main>
 	)
 }
