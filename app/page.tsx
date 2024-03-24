2fe125ef0aa4a8bfcb397690c2b99ad16bed0f09
Clickadelic <toby.hopp@gmail.com>
2024-03-24 03:51:17 +0100

    Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-import { Poppins } from "next/font/google"
+import { Poppins } from "next/font/google";
 
-import { cn } from "@/lib/utils"
-import { LoginButton } from "@/components/auth/login-button"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
 
 const font = Poppins({
 	subsets: ["latin"],
 	weight: ["600"]
-})
+});
 
 export default function Home() {
 	return (
 		<main className="flex flex-col h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-900">
 			<div className="space-y-6 text-center">
-				<h1 className={cn("text-6xl font-semibold text-white shadow-sm", font.className)}>🔒Auth</h1>
-				<p className="text-white">A simple authentication service.</p>
+				<h1 className={cn("text-6xl font-semibold text-white shadow-sm", font.className)}>Dashboard</h1>
+				<p className="text-white">Toby&apos;s Dashboard.</p>
 				<LoginButton>
 					<Button variant="secondary" size="lg">
 						Login
@@ -22,5 +22,5 @@ export default function Home() {
 				</LoginButton>
 			</div>
 		</main>
-	)
+	);
 }
