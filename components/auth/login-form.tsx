81ddf6a1d5b037004b937d39167f01134c212cd2
clickadelic <toby.hopp@gmail.com>
2024-03-29 00:00:38 +0100

    Login Messages (error, success)

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,21 +1,24 @@
-"use client";
+"use client"
 
-import * as z from "zod";
+import * as z from "zod"
 
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
-import { LoginSchema } from "@/schemas";
-import { Input } from "@/components/ui/input";
-import { Form, FormControl, FormLabel, FormField, FormItem, FormMessage } from "@/components/ui/form";
-import { Button } from "@/components/ui/button";
+import { LoginSchema } from "@/schemas"
+import { Input } from "@/components/ui/input"
+import { Form, FormControl, FormLabel, FormField, FormItem, FormMessage } from "@/components/ui/form"
 
-import { CardWrapper } from "./card-wrapper";
+import { CardWrapper } from "./card-wrapper"
+import { Button } from "@/components/ui/button"
+import { FormError } from "@/components/form-error"
+import { FormSuccess } from "@/components/form-success"
+
 export const LoginForm = () => {
-	const form = useForm<z.infer<typeof LoginSchema>>({ resolver: zodResolver(LoginSchema), defaultValues: { email: "", password: "" } });
+	const form = useForm<z.infer<typeof LoginSchema>>({ resolver: zodResolver(LoginSchema), defaultValues: { email: "", password: "" } })
 	const onSubmit = (values: z.infer<typeof LoginSchema>) => {
-		console.log(values);
-	};
+		console.log(values)
+	}
 
 	return (
 		<CardWrapper headerLabel="Willkommen!" backButtonLabel="Noch keinen Account?" backButtonHref="/auth/register" showSocial>
@@ -49,11 +52,13 @@ export const LoginForm = () => {
 							)}
 						/>
 					</div>
+					<FormError />
+					<FormSuccess />
 					<Button type="submit" className="w-full">
 						Login
 					</Button>
 				</form>
 			</Form>
 		</CardWrapper>
-	);
-};
+	)
+}
