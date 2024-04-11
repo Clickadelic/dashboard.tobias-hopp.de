d0ad28d07ea2b3702fca64a58ad717ce1d109c68
clickadelic <toby.hopp@gmail.com>
2024-04-11 23:40:19 +0200

    bcrypt installed, register working

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,36 @@
 "use server"
+import { db } from "@/lib/db"
 
+import bcrypt from "bcryptjs"
 import * as z from "zod"
-
 import { RegisterSchema } from "@/schemas"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	const validatedFields = RegisterSchema.safeParse(values)
 
 	if (!validatedFields.success) {
-		return { error: "Invalid fields!" }
+		return { error: "Ungültige Felder." }
 	}
 
-	return { success: "E-Mail sent!" }
+	const { email, password, name } = validatedFields.data
+
+	const hashedPassword = await bcrypt.hash(password, 10)
+	const existingUser = await db.user.findUnique({
+		where: {
+			email
+		}
+	})
+
+	if (existingUser) {
+		return { error: "E-Mail bereits in Verwendung." }
+	}
+
+	await db.user.create({
+		data: {
+			email,
+			name,
+			password: hashedPassword
+		}
+	})
+	return { success: "Benutzer erstellt." }
 }
