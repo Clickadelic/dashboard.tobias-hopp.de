e13169abf68e300284f4fa9191d06bfd7d1978f1
clickadelic <toby.hopp@gmail.com>
2024-04-28 00:35:51 +0200

    fix?

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,37 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth"
 
-import authConfig from "@/auth.config";
+import authConfig from "@/auth.config"
 
-import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from "@/routes";
+import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from "@/routes"
 
-const { auth } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig)
 
 export default auth(req => {
-	const { nextUrl } = req;
-	const isLoggedIn = !!req.auth;
+	const { nextUrl } = req
+	const isLoggedIn = !!req.auth
 
-	const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-	const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-	const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+	const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+	const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+	const isAuthRoute = authRoutes.includes(nextUrl.pathname)
 
 	if (isApiAuthRoute) {
-		return null;
+		return
 	}
 
 	if (isAuthRoute) {
 		if (isLoggedIn) {
-			return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+			return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
 		}
-		return null;
+		return
 	}
 
 	if (!isLoggedIn && !isPublicRoute) {
-		return Response.redirect(new URL("/auth/login", nextUrl));
+		return Response.redirect(new URL("/auth/login", nextUrl))
 	}
 
-	return null;
-});
+	return
+})
 
 export const config = {
 	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"]
-};
+}
