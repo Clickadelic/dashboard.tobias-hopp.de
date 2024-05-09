ef5882865a465371ca99f53080b1189a8d282fcd
clickadelic <toby.hopp@gmail.com>
2024-05-09 23:10:28 +0200

    Register

--- actions/register.ts
+++ actions/register.ts
@@ -1,27 +1,27 @@
-"use server";
-import * as z from "zod";
-import { RegisterSchema } from "@/schemas";
-import bcrypt from "bcryptjs";
-import { db } from "@/lib/db";
+"use server"
+import * as z from "zod"
+import { RegisterSchema } from "@/schemas"
+import bcrypt from "bcryptjs"
+import { db } from "@/lib/db"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-	const validatedFields = RegisterSchema.safeParse(values);
+	const validatedFields = RegisterSchema.safeParse(values)
 
 	if (!validatedFields.success) {
-		return { error: "Ungültige Felder." };
+		return { error: "Ungültige Felder!" }
 	}
 
-	const { email, password, name } = validatedFields.data;
+	const { email, password, name } = validatedFields.data
 
-	const hashedPassword = await bcrypt.hash(password, 10);
+	const hashedPassword = await bcrypt.hash(password, 10)
 	const existingUser = await db.user.findUnique({
 		where: {
 			email
 		}
-	});
+	})
 
 	if (existingUser) {
-		return { error: "E-Mail bereits in Verwendung!" };
+		return { error: "E-Mail bereits in Verwendung!" }
 	}
 
 	await db.user.create({
@@ -30,8 +30,8 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 			name,
 			password: hashedPassword
 		}
-	});
+	})
 
 	// Send verification token E-Mail
-	return { success: "Registrierung erfolgreich!" };
-};
+	return { success: "Registrierung erfolgreich!" }
+}
