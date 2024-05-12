6c95be5226ceb0d6b3f34fd0244f32dcfb8117a7
Clickadelic <toby.hopp@gmail.com>
2024-05-12 04:29:08 +0200

    temp WIP

--- auth.config.ts
+++ auth.config.ts
@@ -1,30 +1,37 @@
-import Credentials from "next-auth/providers/credentials"
-import bcrypt from "bcryptjs"
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
-import type { NextAuthConfig } from "next-auth"
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import type { NextAuthConfig } from "next-auth";
 
-import { LoginSchema } from "@/schemas"
-import { getUserByEmail } from "./data/user"
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "./data/user";
 
 export default {
 	providers: [
+		GitHub({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET
+		}),
+		Google({}),
 		Credentials({
 			async authorize(credentials) {
-				const validatedFields = LoginSchema.safeParse(credentials)
+				const validatedFields = LoginSchema.safeParse(credentials);
 
 				if (validatedFields.success) {
-					const { email, password } = validatedFields.data
+					const { email, password } = validatedFields.data;
 
-					const user = await getUserByEmail(email)
-					if (!user || !user.password) return null
+					const user = await getUserByEmail(email);
+					if (!user || !user.password) return null;
 
-					const passwordsMatch = await bcrypt.compare(password, user.password)
+					const passwordsMatch = await bcrypt.compare(password, user.password);
 
-					if (passwordsMatch) return user
+					if (passwordsMatch) return user;
 				}
 
-				return null
+				return null;
 			}
 		})
 	]
-} satisfies NextAuthConfig
+} satisfies NextAuthConfig;
