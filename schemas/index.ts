e570bfb0f0030b5dce918eccccdc7b96edcf4479
clickadelic <toby.hopp@gmail.com>
2024-05-23 21:46:00 +0200

    Password optional message removed

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,5 @@
-import * as z from "zod";
-import { UserRole } from "@prisma/client";
+import * as z from "zod"
+import { UserRole } from "@prisma/client"
 
 export const SettingsSchema = z
 	.object({
@@ -7,16 +7,16 @@ export const SettingsSchema = z
 		isTwoFactorEnabled: z.optional(z.boolean()),
 		role: z.enum([UserRole.ADMIN, UserRole.USER]),
 		email: z.optional(z.string().email()),
-		password: z.optional(z.string().min(6, "Mindestens 6 Buchstaben.")),
-		newPassword: z.optional(z.string().min(6, "Mindestens 6 Buchstaben"))
+		password: z.optional(z.string().min(6)),
+		newPassword: z.optional(z.string().min(6))
 	})
 	.refine(
 		data => {
 			if (data.password && !data.newPassword) {
-				return false;
+				return false
 			}
 
-			return true;
+			return true
 		},
 		{
 			message: "Neues Passwort ist notwendig.",
@@ -26,32 +26,32 @@ export const SettingsSchema = z
 	.refine(
 		data => {
 			if (data.newPassword && !data.password) {
-				return false;
+				return false
 			}
-			return true;
+			return true
 		},
 		{
 			message: "Altes Passwort ist notwendig.",
 			path: ["password"]
 		}
-	);
+	)
 
 export const LoginSchema = z.object({
 	email: z.string().email({ message: "E-Mail ist notwendig." }),
 	password: z.string().min(1, { message: "Passwort ist notwendig." }),
 	code: z.optional(z.string())
-});
+})
 
 export const RegisterSchema = z.object({
 	email: z.string().email({ message: "E-Mail ist ein Pflichtfeld." }),
 	password: z.string().min(6, { message: "Mindestens 6 Buchstaben." }),
 	name: z.string().min(1, { message: "Name ist ein Pflichtfeld." })
-});
+})
 
 export const ResetSchema = z.object({
 	email: z.string().email({ message: "E-Mail ist notwendig." })
-});
+})
 
 export const NewPasswordSchema = z.object({
 	password: z.string().min(6, { message: "Mindestens 6 Buchstaben." })
-});
+})
