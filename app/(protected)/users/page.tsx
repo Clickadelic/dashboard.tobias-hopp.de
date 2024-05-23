0ed1ac1e7204f762f220732a8bfdb1350b216039
clickadelic <toby.hopp@gmail.com>
2024-05-23 22:04:40 +0200

    Role Gate test

--- app/(protected)/users/page.tsx
+++ app/(protected)/users/page.tsx
@@ -1,5 +1,17 @@
-const UsersPage = () => {
-	return <div>UsersPage</div>;
-};
+"use client"
 
-export default UsersPage;
+import { useCurrentUser } from "@/hooks/use-current-user"
+import { UserRole } from "@prisma/client"
+import { RoleGate } from "@/components/auth/role-gate"
+
+const UsersPage = () => {
+	const user = useCurrentUser()
+
+	return (
+		<div>
+			<RoleGate allowedRole={UserRole.ADMIN}>Super Secret Admin Content</RoleGate>
+		</div>
+	)
+}
+
+export default UsersPage
