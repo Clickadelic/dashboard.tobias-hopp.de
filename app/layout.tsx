68bbac4d81008ae984c9ff8a7f1b5df5cb8f810a
clickadelic <toby.hopp@gmail.com>
2024-05-24 21:06:14 +0200

    Session added again :(

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,9 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { SessionProvider } from "next-auth/react"
-
+import { auth } from "@/auth"
 import { siteConfig } from "@/config"
 import { Toaster } from "@/components/ui/sonner"
-
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -27,8 +26,9 @@ export default async function RootLayout({
 }: Readonly<{
 	children: React.ReactNode
 }>) {
+	const session = await auth()
 	return (
-		<SessionProvider>
+		<SessionProvider session={session}>
 			<html lang="de">
 				<body className={inter.className}>
 					<Toaster />
