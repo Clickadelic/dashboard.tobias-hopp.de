056de6a8502e02504b0e6735420e593cb4e11a25
Clickadelic <toby.hopp@gmail.com>
2024-05-31 20:07:33 +0200

    Page Clean Up

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
-import { Poppins } from "next/font/google";
-
-import { cn } from "@/lib/utils";
 import { LoginForm } from "@/components/auth/login-form";
 import { Suspense } from "react";
 
-const font = Poppins({
-	subsets: ["latin"],
-	weight: ["600"]
-});
-
-export default function Home() {
+const Home = () => {
 	return (
 		<main className="flex flex-col h-full items-center justify-center bg-slate-800">
 			<div className="space-y-6 text-center">
 				<div className="text-left">
-					<Suspense fallback={<div>Loading...</div>}>
+					<Suspense fallback={<div className="text-white text-center">Loading...</div>}>
 						<LoginForm />
 					</Suspense>
 				</div>
 			</div>
 		</main>
 	);
-}
+};
+
+export default Home;
