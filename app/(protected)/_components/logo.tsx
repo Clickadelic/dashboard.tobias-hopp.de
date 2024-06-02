9c419df8013dbab4c8e4e2257223d525e5af4e93
clickadelic <toby.hopp@gmail.com>
2024-06-02 21:05:17 +0200

    logo font LaBelleAurore

--- app/(protected)/_components/logo.tsx
+++ app/(protected)/_components/logo.tsx
@@ -2,14 +2,18 @@ import Image from "next/image"
 import Link from "next/link"
 import logoSrc from "@/public/favicon.svg"
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
+import { cn } from "@/lib/utils"
 
+import { La_Belle_Aurore } from "next/font/google"
+
+const laBelleAurore = La_Belle_Aurore({ subsets: ["latin"], weight: ["400"] })
 const Logo = () => {
 	return (
 		<h1>
 			<Link href={DEFAULT_LOGIN_REDIRECT} className="flex justify-between mt-2 text-slate-900 hover:opacity-75">
 				<Image src={logoSrc} width={16} height={16} className="logo inline -mt-1 h-8 w-8" alt="Tailwind Dashboard" />
 				<span id="tailwind-dashboard" className="ml-2">
-					<span className="md:inline-block font-medium mr-1">Toby&apos;s</span>
+					<span className={cn("md:inline-block font-medium mr-1 text-xl", laBelleAurore.className)}>Toby&apos;s</span>
 					<span className="md:inline-block font-bold">Dashboard</span>
 				</span>
 			</Link>
