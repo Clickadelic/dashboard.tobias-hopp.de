16d33de0a3b0506719bfacd879d8173e6ce0adef
clickadelic <toby.hopp@gmail.com>
2024-06-02 02:42:51 +0200

    Markup entfernt LinkCounter

--- app/(protected)/_components/link-counter.tsx
+++ app/(protected)/_components/link-counter.tsx
@@ -2,5 +2,5 @@ import { countLinks } from "@/data/links"
 
 export const LinkCounter = () => {
 	const links = countLinks()
-	return <h3 className="text-sm mb-2">{links} Links</h3>
+	return links
 }
