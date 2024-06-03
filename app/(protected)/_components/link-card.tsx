07eb1c9c87fb22482354aa5b1eb58648fad62e9e
Clickadelic <toby.hopp@gmail.com>
2024-06-03 09:45:42 +0200

    Link Count added (Potential Error)

--- app/(protected)/_components/link-card.tsx
+++ app/(protected)/_components/link-card.tsx
@@ -17,6 +17,7 @@ import { Button } from "@/components/ui/button";
 import { FiPlus } from "react-icons/fi";
 
 import { addLink } from "@/actions/links/add-link";
+import { countLinks } from "@/data/links";
 
 const LinkCard = () => {
 	const [isPending, startTransiton] = useTransition();
@@ -50,7 +51,8 @@ const LinkCard = () => {
 
 	return (
 		<div className="bg-white rounded shadow-sm border px-3 py-4">
-			<h2 className="text-sm border-bottom">Links</h2>
+			<h2 className="text-sm border-bottom">Links Total</h2>
+			<h3 className="text-2xl my-3">{countLinks()}</h3>
 			<Popover>
 				<PopoverTrigger className="flex justify-center w-full px-3 py-2 bg-slate-100 text-slate-700 hover:text-slate-800 hover:bg-slate-200 text-xs md:text-base rounded-sm mb-1">
 					<FiPlus className="mt-[.125rem] md:mt-1 mr-2" /> Link hinzufügen
