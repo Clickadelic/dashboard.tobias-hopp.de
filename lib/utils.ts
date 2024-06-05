20c58aba4717796af016e82c25e0453d827c6461
Clickadelic <toby.hopp@gmail.com>
2024-06-05 06:04:44 +0200

    Benutzer löschen geht :-) - Feierabend

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function capitalizeFirstLetter(string: string) {
+	string = string.toLowerCase();
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function slugify(s: string) {
+	s = s
+		.toLowerCase()
+		.replace(/[^a-z0-9 -]/g, "")
+		.replace(/\s+/g, "-")
+		.replace(/-+/g, "-");
+	return s;
+}
