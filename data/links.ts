130e308679785820cdd9e1ad9aadf76948dfa819
clickadelic <toby.hopp@gmail.com>
2024-06-06 04:22:30 +0200

    useless file removed

--- data/links.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/lib/db";
-
-export const countLinks = async () => {
-	return await db.link.count();
-};
-
-export const getLinks = async () => {
-	return await db.link.findMany();
-};
