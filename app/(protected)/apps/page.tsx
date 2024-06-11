41ddecdf7aaa2daac81521758af008ec58c6e2dc
Clickadelic <toby.hopp@gmail.com>
2024-06-11 02:17:36 +0200

    isPinned removed

---
+++ app/(protected)/apps/page.tsx
@@ -0,0 +1,10 @@
+const AppsPage = () => {
+	return (
+		<div className="page-wrapper">
+			<h2 className="text-md font-bold text-slate-700 mb-5">Apps</h2>
+			<div className="bg-white rounded shadow-sm border p-3">Lege die Apps/Links für die Startseite fest.</div>
+		</div>
+	);
+};
+
+export default AppsPage;
---
+++ app/(protected)/organisationen/page.tsx
@@ -0,0 +1,10 @@
+const OrganizationsPage = () => {
+	return (
+		<div className="page-wrapper">
+			<h2 className="text-md font-bold text-slate-700 mb-5">Organisationen</h2>
+			<div className="bg-white rounded shadow-sm border p-3">Feature für Organisationen zu bilden.</div>
+		</div>
+	);
+};
+
+export default OrganizationsPage;
