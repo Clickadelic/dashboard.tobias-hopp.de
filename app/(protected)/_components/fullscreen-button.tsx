c1365b33b7af28ed6212073a4bb8032c44818fbe
Clickadelic <toby.hopp@gmail.com>
2024-06-12 03:34:26 +0200

    Fullscreen Button in/out

--- app/(protected)/_components/fullscreen-button.tsx
+++ app/(protected)/_components/fullscreen-button.tsx
@@ -1,28 +1,34 @@
 "use client";
+import { useState, useEffect } from "react";
 import { BsArrowsFullscreen } from "react-icons/bs";
 import { AiOutlineFullscreenExit } from "react-icons/ai";
 
 export const FullscreenButton = () => {
-	let isFullscreen = false;
+	const [isFullscreen, setIsFullscreen] = useState(false);
 
-	function requestFullscreen() {
+	useEffect(() => {
+		const handleFullscreenChange = () => {
+			setIsFullscreen(!!document.fullscreenElement);
+		};
+
+		document.addEventListener("fullscreenchange", handleFullscreenChange);
+
+		return () => {
+			document.removeEventListener("fullscreenchange", handleFullscreenChange);
+		};
+	}, []);
+
+	const requestFullscreen = () => {
 		if (document.fullscreenElement) {
-			isFullscreen = false;
 			document.exitFullscreen().catch(err => console.error(err));
 		} else {
-			isFullscreen = true;
-			document.documentElement.requestFullscreen();
+			document.documentElement.requestFullscreen().catch(err => console.error(err));
 		}
-	}
+	};
 
 	return (
 		<li>
-			<button
-				onClick={() => {
-					requestFullscreen();
-				}}
-				className="hover:bg-slate-200 mx-1 inline-flex p-3 rounded"
-			>
+			<button onClick={requestFullscreen} className="hover:bg-slate-200 mx-1 inline-flex p-3 rounded">
 				{isFullscreen ? <AiOutlineFullscreenExit /> : <BsArrowsFullscreen />}
 			</button>
 		</li>
