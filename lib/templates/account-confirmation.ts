19a57a59a3e27cb0ac2350fbe7270c7c2b585704
clickadelic <toby.hopp@gmail.com>
2024-07-07 17:04:57 +0200

    Account Confirmation Template Overhaul

--- lib/templates/account-confirmation.ts
+++ lib/templates/account-confirmation.ts
@@ -42,8 +42,10 @@ export const accountConfirmationTemplate = `<!DOCTYPE html>
 			display: none;
 		}
 
-		@media (max-width:700px) {
-			.desktop_hide table.icons-inner {
+		@media (max-width:500px) {
+
+			.desktop_hide table.icons-inner,
+			.social_block.desktop_hide .social-table {
 				display: inline-block !important;
 			}
 
@@ -102,14 +104,14 @@ export const accountConfirmationTemplate = `<!DOCTYPE html>
 						<tbody>
 							<tr>
 								<td>
-									<table class="row-content" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #1e293b; color: #000000; width: 680px; margin: 0 auto;" width="680">
+									<table class="row-content" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #1e293b; color: #000000; width: 480px; margin: 0 auto;" width="480">
 										<tbody>
 											<tr>
 												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 10px; padding-top: 20px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
 													<table class="heading_block block-1" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
 														<tr>
 															<td class="pad">
-																<h2 style="margin: 0; color: #ffffff; direction: ltr; font-family: Poppins, Arial, Helvetica, sans-serif; font-size: 22px; font-weight: 400; letter-spacing: normal; line-height: 150%; text-align: center; margin-top: 0; margin-bottom: 0; mso-line-height-alt: 33px;"><span class="tinyMce-placeholder">Deine Registrierung</span></h2>
+																<h2 style="margin: 0; color: #ffffff; direction: ltr; font-family: Poppins, Arial, Helvetica, sans-serif; font-size: 22px; font-weight: 400; letter-spacing: normal; line-height: 150%; text-align: center; margin-top: 0; margin-bottom: 0; mso-line-height-alt: 33px;"><span class="tinyMce-placeholder" style="word-break: break-word;">Deine Registrierung</span></h2>
 															</td>
 														</tr>
 													</table>
@@ -125,7 +127,7 @@ export const accountConfirmationTemplate = `<!DOCTYPE html>
 						<tbody>
 							<tr>
 								<td>
-									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-size: auto; background-color: #ffffff; border-radius: 30px 30px 0 0; color: #000000; width: 680px; margin: 0 auto;" width="680">
+									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-size: auto; background-color: #ffffff; border-radius: 30px 30px 0 0; color: #000000; width: 480px; margin: 0 auto;" width="480">
 										<tbody>
 											<tr>
 												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
@@ -134,15 +136,15 @@ export const accountConfirmationTemplate = `<!DOCTYPE html>
 														<tr>
 															<td class="pad" style="width:100%;">
 																<div class="alignment" align="center" style="line-height:10px">
-																	<div style="max-width: 251px;"><img src="process.env.NEXT_PUBLIC_APP_URL/images/email/Logo-Tobys-Dashboard.jpg" style="display: block; height: auto; border: 0; width: 100%;" width="251" height="auto"></div>
+																	<div style="max-width: 251px;"><img src="https://6f888fb6f3.imgdist.com/pub/bfra/b3dk3mbl/vez/ama/iuu/Logo-Tobys-Dashboard.jpg" style="display: block; height: auto; border: 0; width: 100%;" width="251" height="auto"></div>
 																</div>
 															</td>
 														</tr>
 													</table>
 													<table class="heading_block block-3" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
 														<tr>
-															<td class="pad" style="padding-bottom:30px;padding-left:60px;padding-right:60px;padding-top:30px;text-align:center;width:100%;">
-																<h1 style="margin: 0; color: #020b22; direction: ltr; font-family: Poppins, Arial, Helvetica, sans-serif; font-size: 22px; font-weight: 700; letter-spacing: normal; line-height: 150%; text-align: center; margin-top: 0; margin-bottom: 0; mso-line-height-alt: 33px;"><span class="tinyMce-placeholder">Hi, Du hast Dich registriert, richtig?</span></h1>
+															<td class="pad" style="padding-bottom:10px;padding-left:60px;padding-right:60px;padding-top:5px;text-align:center;width:100%;">
+																<h1 style="margin: 0; color: #020b22; direction: ltr; font-family: Poppins, Arial, Helvetica, sans-serif; font-size: 22px; font-weight: 700; letter-spacing: normal; line-height: 150%; text-align: center; margin-top: 0; margin-bottom: 0; mso-line-height-alt: 33px;"><span class="tinyMce-placeholder" style="word-break: break-word;">Hi, Du hast Dich registriert, richtig?</span></h1>
 															</td>
 														</tr>
 													</table>
@@ -158,7 +160,7 @@ export const accountConfirmationTemplate = `<!DOCTYPE html>
 						<tbody>
 							<tr>
 								<td>
-									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff; color: #000000; width: 680px; margin: 0 auto;" width="680">
+									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff; color: #000000; width: 480px; margin: 0 auto;" width="480">
 										<tbody>
 											<tr>
 												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
@@ -166,9 +168,9 @@ export const accountConfirmationTemplate = `<!DOCTYPE html>
 														<tr>
 															<td class="pad" style="padding-left:30px;padding-right:30px;">
 																<div style="color:#101112;direction:ltr;font-family:Poppins, Arial, Helvetica, sans-serif;font-size:16px;font-weight:400;letter-spacing:0px;line-height:150%;text-align:left;mso-line-height-alt:24px;">
-																	<p style="margin: 0; margin-bottom: 16px;">Danke für Deine Registrierung, cool - 😎.</p>
-																	<p style="margin: 0; margin-bottom: 16px;">Sei doch so nett, und verifiziere kurz Deinen Account bzw. Deine E-Mail Adresse mit einem Klick auf den unten stehen Link.</p>
-																	<p style="margin: 0;">Danach stehen Dir alle Funktionen des Dashboard's zur Verfügung.</p>
+																	<p style="margin: 0; margin-bottom: 16px;">Danke für Deine Registrierung. 😎</p>
+																	<p style="margin: 0; margin-bottom: 16px;">Bitte sei so nett, und verifiziere kurz Deinen Account bzw. Deine E-Mail Adresse mit einem Klick auf den unten stehenden Link.</p>
+																	<p style="margin: 0;">Danach stehen Dir alle Funktionen meines Dashboard's zur Verfügung.</p>
 																</div>
 															</td>
 														</tr>
@@ -182,7 +184,7 @@ export const accountConfirmationTemplate = `<!DOCTYPE html>
 <w:anchorlock/>
 <v:textbox inset="0px,0px,0px,0px">
 <center dir="false" style="color:#ffffff;font-family:Arial, sans-serif;font-size:16px">
-<![endif]--><a href="{{confirmationLink}}" target="_blank" style="background-color:#000000;border-bottom:0px solid transparent;border-left:0px solid transparent;border-radius:4px;border-right:0px solid transparent;border-top:0px solid transparent;color:#ffffff;display:inline-block;font-family:Poppins, Arial, Helvetica, sans-serif;font-size:16px;font-weight:undefined;mso-border-alt:none;padding-bottom:5px;padding-top:5px;text-align:center;text-decoration:none;width:auto;word-break:keep-all;"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="margin: 0; word-break: break-word; line-height: 32px;">Account bestätigen</span></span></a><!--[if mso]></center></v:textbox></v:roundrect><![endif]--></div>
+<![endif]--><a href="{{confirmationLink}}" target="_blank" style="background-color:#000000;border-bottom:0px solid transparent;border-left:0px solid transparent;border-radius:4px;border-right:0px solid transparent;border-top:0px solid transparent;color:#ffffff;display:inline-block;font-family:Poppins, Arial, Helvetica, sans-serif;font-size:16px;font-weight:undefined;mso-border-alt:none;padding-bottom:5px;padding-top:5px;text-align:center;text-decoration:none;width:auto;word-break:keep-all;"><span style="word-break: break-word; padding-left: 20px; padding-right: 20px; font-size: 16px; display: inline-block; letter-spacing: normal;"><span style="margin: 0; word-break: break-word; line-height: 32px;">Account bestätigen</span></span></a><!--[if mso]></center></v:textbox></v:roundrect><![endif]--></div>
 															</td>
 														</tr>
 													</table>
@@ -198,10 +200,10 @@ export const accountConfirmationTemplate = `<!DOCTYPE html>
 						<tbody>
 							<tr>
 								<td>
-									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff; border-radius: 0 0 30px 30px; color: #000000; width: 680px; margin: 0 auto;" width="680">
+									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff; border-radius: 0 0 30px 30px; color: #000000; width: 480px; margin: 0 auto;" width="480">
 										<tbody>
 											<tr>
-												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 15px; padding-top: 15px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
 													<table class="paragraph_block block-1" width="100%" border="0" cellpadding="25" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
 														<tr>
 															<td class="pad">
@@ -226,17 +228,38 @@ export const accountConfirmationTemplate = `<!DOCTYPE html>
 						<tbody>
 							<tr>
 								<td>
-									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #1e293b; color: #000000; width: 680px; margin: 0 auto;" width="680">
+									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #1e293b; color: #000000; width: 480px; margin: 0 auto;" width="480">
 										<tbody>
 											<tr>
 												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 25px; padding-top: 25px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
-													<table class="paragraph_block block-1" width="100%" border="0" cellpadding="20" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+													<table class="social_block block-1" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
 														<tr>
 															<td class="pad">
-																<div style="color:#fafafa;font-family:Poppins, Arial, Helvetica, sans-serif;font-size:14px;font-weight:400;line-height:150%;text-align:center;mso-line-height-alt:15px;">
-																	<p style="margin: 0;"><a href="https://www.tobias-hopp.de/" target="_blank" style="text-decoration:underline;color:#ffffff;" title="Webseite">Tobias Hopp</a></p>
+																<div class="alignment" align="center">
+																	<table class="social-table" width="144px" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: inline-block;">
+																		<tr>
+																			<td style="padding:0 2px 0 2px;"><a href="https://www.linkedin.comhttps://www.linkedin.com/in/tobias-hopp-9464262ab//" target="_blank"><img src="https://app-rsrc.getbee.io/public/resources/social-networks-icon-sets/colored/linkedin@2x.png" width="32" height="auto" alt="Linkedin" title="LinkedIn" style="display: block; height: auto; border: 0;"></a></td>
+																			<td style="padding:0 2px 0 2px;"><a href="https://t.me/clickadelic" target="_blank"><img src="https://app-rsrc.getbee.io/public/resources/social-networks-icon-sets/colored/telegram@2x.png" width="32" height="auto" alt="Telegram" title="Telegram" style="display: block; height: auto; border: 0;"></a></td>
+																			<td style="padding:0 2px 0 2px;"><a href="tel:#4915788080947" target="_blank"><img src="https://app-rsrc.getbee.io/public/resources/social-networks-icon-sets/colored/whatsapp@2x.png" width="32" height="auto" alt="WhatsApp" title="WhatsApp" style="display: block; height: auto; border: 0;"></a></td>
+																			<td style="padding:0 2px 0 2px;"><a href="mailto:mailto:mail@tobias-hopp.de" target="_blank"><img src="https://app-rsrc.getbee.io/public/resources/social-networks-icon-sets/colored/mail@2x.png" width="32" height="auto" alt="E-Mail" title="E-Mail" style="display: block; height: auto; border: 0;"></a></td>
+																		</tr>
+																	</table>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table class="paragraph_block block-2" width="100%" border="0" cellpadding="20" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+														<tr>
+															<td class="pad">
+																<div style="color:#fafafa;font-family:Poppins, Arial, Helvetica, sans-serif;font-size:12px;font-weight:400;line-height:150%;text-align:center;mso-line-height-alt:18px;">
+																	<p style="margin: 0;">· Tobias Hopp ·</p>
 																	<p style="margin: 0;">Full-Stack Developer</p>
 																	<p style="margin: 0;">Fremdsprachenkorrespondent</p>
+																	<p style="margin: 0;">&nbsp;</p>
+																	<p style="margin: 0;"><a href="https://www.tobias-hopp.de" target="_blank" title="Webseite" style="text-decoration: underline; color: #ffffff;" rel="noopener">https://www.tobias-hopp.de</a></p>
+																	<p style="margin: 0;"><a href="https://dashboard.tobias-hopp.de" target="_blank" title="Toby's Dashboard" style="text-decoration: underline; color: #ffffff;" rel="noopener">https://dashboard.tobias-hopp.de</a></p>
+																	<p style="margin: 0;">&nbsp;</p>
+																	<p style="margin: 0;">Alle Rechte vorbehalten © 2024.</p>
 																</div>
 															</td>
 														</tr>
@@ -255,4 +278,4 @@ export const accountConfirmationTemplate = `<!DOCTYPE html>
 	</table><!-- End -->
 </body>
 
-</html>`;
+</html>`
