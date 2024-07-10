2bf3559eb5d12cd654339ba462c6000d3992feba
Clickadelic <toby.hopp@gmail.com>
2024-07-10 03:17:57 +0200

    bg-fixed

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,15 +1,15 @@
-import LayoutContext from "./layout-context"
+import LayoutContext from "./layout-context";
 
 interface ProtectedLayoutProps {
-	children: React.ReactNode
+	children: React.ReactNode;
 }
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
 	return (
-		<div className="min-h-screen bg-default bg-cover flex">
+		<div className="min-h-screen bg-default bg-cover bg-fixed flex">
 			<LayoutContext>{children}</LayoutContext>
 		</div>
-	)
-}
+	);
+};
 
-export default ProtectedLayout
+export default ProtectedLayout;
