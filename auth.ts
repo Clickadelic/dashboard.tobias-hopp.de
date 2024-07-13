6532faba7646a3eeec90bce91b4bfe80436e2c7e
clickadelic <toby.hopp@gmail.com>
2024-07-14 00:04:50 +0200

    Bio Works =)

--- auth.ts
+++ auth.ts
@@ -1,13 +1,13 @@
-import NextAuth from "next-auth";
-import authConfig from "@/auth.config";
-import { UserRole } from "@prisma/client";
+import NextAuth from "next-auth"
+import authConfig from "@/auth.config"
+import { UserRole } from "@prisma/client"
 
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { db } from "@/lib/db";
-import { getUserById } from "@/data/user";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { db } from "@/lib/db"
+import { getUserById } from "@/data/user"
 
-import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
-import { getAccountByUserId } from "./data/account";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation"
+import { getAccountByUserId } from "./data/account"
 
 export const {
 	handlers: { GET, POST },
@@ -24,73 +24,75 @@ export const {
 			await db.user.update({
 				where: { id: user.id },
 				data: { emailVerified: new Date() }
-			});
+			})
 		}
 	},
 	callbacks: {
 		async signIn({ user, account }) {
-			if (account?.provider !== "credentials") return true;
+			if (account?.provider !== "credentials") return true
 
-			const existingUser = await getUserById(user.id!);
+			const existingUser = await getUserById(user.id!)
 
 			// Prevent Login without E-Mail verification
-			if (!existingUser?.emailVerified) return false;
+			if (!existingUser?.emailVerified) return false
 
 			if (existingUser.isTwoFactorEnabled) {
-				const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
+				const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id)
 
-				if (!twoFactorConfirmation) return false;
+				if (!twoFactorConfirmation) return false
 
 				await db.twoFactorConfirmation.delete({
 					where: { id: twoFactorConfirmation.id }
-				});
+				})
 			}
 
-			return true;
+			return true
 		},
 		async session({ token, session }) {
 			if (token.sub && session.user) {
-				session.user.id = token.sub;
+				session.user.id = token.sub
 			}
 
 			if (token.role && session.user) {
-				session.user.role = token.role as UserRole;
+				session.user.role = token.role as UserRole
 			}
 
 			if (session.user) {
-				session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+				session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
 			}
 
 			if (session.user) {
-				session.user.name = token.name;
-				session.user.profileImage = token.profileImage as string | null;
-				session.user.backgroundImage = token.backgroundImage as string | null;
-				session.user.email = token.email!;
-				session.user.isOAuth = token.isOAuth as boolean;
+				session.user.name = token.name
+				session.user.bio = token.bio as string | null
+				session.user.profileImage = token.profileImage as string | null
+				session.user.backgroundImage = token.backgroundImage as string | null
+				session.user.email = token.email!
+				session.user.isOAuth = token.isOAuth as boolean
 			}
 
-			return session;
+			return session
 		},
 		async jwt({ token }) {
-			if (!token.sub) return token;
+			if (!token.sub) return token
 
-			const existingUser = await getUserById(token.sub);
+			const existingUser = await getUserById(token.sub)
 
-			if (!existingUser) return token;
+			if (!existingUser) return token
 
-			const existingAccount = await getAccountByUserId(existingUser.id);
+			const existingAccount = await getAccountByUserId(existingUser.id)
 
-			token.isOAuth = !!existingAccount;
-			token.name = existingUser.name;
-			token.profileImage = existingUser.profileImage;
-			token.backroundImage = existingUser.backgroundImage;
-			token.email = existingUser.email;
-			token.role = existingUser.role;
-			token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
-			return token;
+			token.isOAuth = !!existingAccount
+			token.name = existingUser.name
+			token.bio = existingUser.bio
+			token.profileImage = existingUser.profileImage
+			token.backroundImage = existingUser.backgroundImage
+			token.email = existingUser.email
+			token.role = existingUser.role
+			token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
+			return token
 		}
 	},
 	adapter: PrismaAdapter(db) as any,
 	session: { strategy: "jwt" },
 	...authConfig
-});
+})
