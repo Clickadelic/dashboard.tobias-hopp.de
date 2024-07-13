1029a547c986720c8eb7d60e87631d7a32c3fb3e
clickadelic <toby.hopp@gmail.com>
2024-07-13 05:06:30 +0200

    ChatGPT Preps

--- app/(protected)/_components/cockpit.tsx
+++ app/(protected)/_components/cockpit.tsx
@@ -4,11 +4,14 @@ import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, Dr
 import { Button } from "@/components/ui/button"
 import { IoMdClose } from "react-icons/io"
 import { BsChatRightText } from "react-icons/bs"
+import { BsSend } from "react-icons/bs"
+import { PiEyesLight } from "react-icons/pi"
+
 export const Cockpit = () => {
 	return (
 		<Drawer>
 			<DrawerTrigger className="fixed bottom-0 w-full">
-				<div className="w-40 bg-black text-white p-2 rounded-tl-lg rounded-tr-lg mx-auto hover:bg-black/90">
+				<div className="w-40 bg-mantis-primary border hover:border-mantis-primary border-b-0 text-white p-2 rounded-tl-lg rounded-tr-lg mx-auto hover:bg-black/90">
 					<BsChatRightText className="inline-block mr-2 mt-[-3px]" /> Ai-Cockpit
 				</div>
 			</DrawerTrigger>
@@ -19,14 +22,24 @@ export const Cockpit = () => {
 					</Button>
 				</DrawerClose>
 				<DrawerHeader className="container mx-auto">
-					<DrawerTitle>Chat GPT</DrawerTitle>
+					<DrawerTitle>
+						<BsChatRightText className="inline-block mr-2 mt-[-3px]" /> Chat GPT
+					</DrawerTitle>
 					<DrawerDescription>Deine Schaltzentrale</DrawerDescription>
 				</DrawerHeader>
 				<div className="min-h-[500px] container mx-auto">
-					<div className="flex items-center justify-between space-x-2">Cockpit content</div>
+					<div className="flex items-center justify-between space-x-2">ChatGPTs antwort</div>
 				</div>
 				<DrawerFooter className="container mx-auto mb-12">
-					<Button>Submit</Button>
+					<form className="w-full flex justify-between">
+						<span className="rounded-tl-full rounded-bl-full border border-r-0 p-2">
+							<BsChatRightText className="mt-2 ml-3 inline-block text-slate-500" />
+						</span>
+						<input type="text" placeholder="Deine Frage an ChatGPT..." className="border-l-0 border-r-0 w-full border p-3" />
+						<button type="submit" className="rounded-tr-full rounded-br-full border border-l-0 p-2">
+							<BsSend className="inline-block mx-3" />
+						</button>
+					</form>
 				</DrawerFooter>
 			</DrawerContent>
 		</Drawer>
