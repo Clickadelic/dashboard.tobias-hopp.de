e9dfa95abc4df2321bd0ca1fdf97bce4ba6fc492
Clickadelic <toby.hopp@gmail.com>
2024-07-13 06:32:46 +0200

    Form error, Form success moved

--- app/(protected)/(examples)/server/page.tsx
+++ app/(protected)/(examples)/server/page.tsx
@@ -1,14 +1,14 @@
-"use server"
-import { auth } from "@/auth"
-import { UserInfo } from "@/components/user-info"
+"use server";
+import { auth } from "@/auth";
+import { UserInfo } from "@/app/(protected)/(examples)/user-info";
 
 const ServerPage = async () => {
-	const session = await auth()
+	const session = await auth();
 	return (
 		<div className="flex flex-col">
 			<UserInfo label="Server Component" user={session?.user} />
 		</div>
-	)
-}
+	);
+};
 
-export default ServerPage
+export default ServerPage;
