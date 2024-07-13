eb7c51df8007c3e5ef5a68cc039f8045c15ca85e
clickadelic <toby.hopp@gmail.com>
2024-07-14 00:20:02 +0200

    import fix

---
+++ app/(protected)/mein/profil/page.tsx
@@ -0,0 +1,12 @@
+import { UserProfileCard } from "@/components/cards/user-profile-card"
+
+const ProfilePage = () => {
+	return (
+		<div className="page-wrapper">
+			<h2 className="text-md font-bold text-slate-700 mb-5">Profil</h2>
+			<UserProfileCard classNames="bg-white/30 backdrop-blur p-2 md:p-4 rounded-xl mb-5 shadow-sm" />
+		</div>
+	)
+}
+
+export default ProfilePage
