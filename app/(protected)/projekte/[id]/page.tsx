4fcd4773596a5b92ec71070a3db34c49ee1cfe6c
Clickadelic <toby.hopp@gmail.com>
2024-07-14 10:04:25 +0200

    form fix

--- app/(protected)/projekte/[id]/page.tsx
+++ app/(protected)/projekte/[id]/page.tsx
@@ -10,7 +10,7 @@ const ProjectByIdPage = async ({ params }: { params: { id: string } }) => {
 			<h2 className="text-md flex justify-start font-bold text-slate-700 mb-5">{project?.title}</h2>
 			<div className="bg-white rounded-xl shadow-sm border p-4">
 				<h3>{project?.title}</h3>
-				<FormProject project={project} />
+				<FormProject />
 			</div>
 		</div>
 	)
