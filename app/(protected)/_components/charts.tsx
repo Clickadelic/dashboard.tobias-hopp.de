3d7a3e635851a3bc49c09dc60f9b6c3c8d7d53e4
clickadelic <toby.hopp@gmail.com>
2024-07-16 22:34:59 +0200

    Some Charts

--- app/(protected)/_components/charts.tsx
+++ app/(protected)/_components/charts.tsx
@@ -6,41 +6,91 @@ import { ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/
 import { BarChart, Bar } from "recharts"
 
 import { useState, useEffect } from "react"
-import { Project } from "@prisma/client"
+
+import { Project, Notice, Todo, Link } from "@prisma/client"
 import { getProjectsByUserId } from "@/actions/project"
+import { getNoticesByUserId } from "@/actions/notice"
+import { getTodosByUserId } from "@/actions/todo"
+import { getLinksByUserId } from "@/actions/link"
 
 const chartConfig = {
 	projects: {
 		label: "Projekte",
 		color: "#1677ff"
+	},
+	notices: {
+		label: "Notizen",
+		color: "#1677ff"
+	},
+	todos: {
+		label: "Todos",
+		color: "#1677ff"
+	},
+	links: {
+		label: "Links",
+		color: "#1677ff"
 	}
 } satisfies ChartConfig
 
 export function Charts() {
-	const chartData = []
-
 	const [isLoading, setIsLoading] = useState<boolean>(false)
 	const [projectCount, setProjectCount] = useState<Project[]>([])
+	const [noticeCount, setNoticeCount] = useState<Notice[]>([])
+	const [todoCount, setTodoCount] = useState<Todo[]>([])
+	const [linkCount, setLinkCount] = useState<Link[]>([])
 
-	const myProjectCount = async () => {
-		const projectResults = await getProjectsByUserId()
-		setProjectCount(projectResults)
-		const month = new Date().getMonth()
-		chartData.push({
-			projects: projectResults?.length
-		})
-		console.log(chartData)
+	const getChartData = async () => {
+		setIsLoading(true)
+		try {
+			const projectResults = await getProjectsByUserId()
+			setProjectCount(projectResults)
+			const noticeResults = await getNoticesByUserId()
+			setNoticeCount(noticeResults)
+			const todoResults = await getTodosByUserId()
+			setTodoCount(todoResults)
+			const linkResults = await getLinksByUserId()
+			setLinkCount(linkResults)
+		} catch (error) {
+			console.log(error)
+		}
+		setIsLoading(false)
 	}
+
+	const chartData = [
+		{
+			name: "Projekte",
+			projects: projectCount.length
+		},
+		{
+			name: "Notizen",
+			notices: noticeCount.length
+		},
+		{
+			name: "Todos",
+			todos: todoCount.length
+		},
+		{
+			name: "Links",
+			links: linkCount.length
+		}
+	]
 
 	useEffect(() => {
 		setIsLoading(true)
-		myProjectCount()
+		getChartData()
 		setIsLoading(false)
 	}, [])
+	console.log("Is it loading?:", isLoading)
 	return (
 		<ChartContainer config={chartConfig} className="min-h-[200px] w-full">
 			<BarChart accessibilityLayer data={chartData}>
-				<Bar dataKey="projects" fill="var(--color-projects)" radius={4} />
+				<Bar dataKey="projects" fill="var(--color-projects)" radius={0} />
+				<ChartTooltip content={<ChartTooltipContent />} />
+				<Bar dataKey="notices" fill="var(--color-notices)" radius={0} />
+				<ChartTooltip content={<ChartTooltipContent />} />
+				<Bar dataKey="todos" fill="var(--color-todos)" radius={0} />
+				<ChartTooltip content={<ChartTooltipContent />} />
+				<Bar dataKey="links" fill="var(--color-links)" radius={0} />
 				<ChartTooltip content={<ChartTooltipContent />} />
 			</BarChart>
 		</ChartContainer>
