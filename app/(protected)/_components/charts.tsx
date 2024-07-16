d408acc46e1ca4bf3c9cd4cda11b716452e87c74
clickadelic <toby.hopp@gmail.com>
2024-07-16 23:00:33 +0200

    charts

--- app/(protected)/_components/charts.tsx
+++ app/(protected)/_components/charts.tsx
@@ -3,7 +3,7 @@
 import { type ChartConfig } from "@/components/ui/chart"
 
 import { ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
-import { BarChart, Bar } from "recharts"
+import { BarChart, Bar, CartesianGrid, XAxis } from "recharts"
 
 import { useState, useEffect } from "react"
 
@@ -12,25 +12,6 @@ import { getProjectsByUserId } from "@/actions/project"
 import { getNoticesByUserId } from "@/actions/notice"
 import { getTodosByUserId } from "@/actions/todo"
 import { getLinksByUserId } from "@/actions/link"
-
-const chartConfig = {
-	projects: {
-		label: "Projekte",
-		color: "#1677ff"
-	},
-	notices: {
-		label: "Notizen",
-		color: "#1677ff"
-	},
-	todos: {
-		label: "Todos",
-		color: "#1677ff"
-	},
-	links: {
-		label: "Links",
-		color: "#1677ff"
-	}
-} satisfies ChartConfig
 
 export function Charts() {
 	const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -75,6 +56,45 @@ export function Charts() {
 		}
 	]
 
+	// const chartData = [
+	// 	{ month: "January", desktop: 186, mobile: 80 },
+	// 	{ month: "February", desktop: 305, mobile: 200 },
+	// 	{ month: "March", desktop: 237, mobile: 120 },
+	// 	{ month: "April", desktop: 73, mobile: 190 },
+	// 	{ month: "May", desktop: 209, mobile: 130 },
+	// 	{ month: "June", desktop: 214, mobile: 140 }
+	// ]
+
+	// const chartConfig = {
+	// 	desktop: {
+	// 		label: "Desktop",
+	// 		color: "#2563eb"
+	// 	},
+	// 	mobile: {
+	// 		label: "Mobile",
+	// 		color: "#60a5fa"
+	// 	}
+	// } satisfies ChartConfig
+
+	const chartConfig = {
+		projects: {
+			label: "Projekte",
+			color: "#1677ff"
+		},
+		notices: {
+			label: "Notizen",
+			color: "#1677ff"
+		},
+		todos: {
+			label: "Todos",
+			color: "#1677ff"
+		},
+		links: {
+			label: "Links",
+			color: "#1677ff"
+		}
+	} satisfies ChartConfig
+
 	useEffect(() => {
 		setIsLoading(true)
 		getChartData()
@@ -82,16 +102,15 @@ export function Charts() {
 	}, [])
 	console.log("Is it loading?:", isLoading)
 	return (
-		<ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+		<ChartContainer config={chartConfig} className="min-h-[200px] flex justify-start items-start">
 			<BarChart accessibilityLayer data={chartData}>
-				<Bar dataKey="projects" fill="var(--color-projects)" radius={0} />
+				<CartesianGrid vertical={false} />
+				<XAxis dataKey="name" tickLine={false} tickMargin={10} axisLine={false} tickFormatter={value => value.slice(0, 3)} />
 				<ChartTooltip content={<ChartTooltipContent />} />
-				<Bar dataKey="notices" fill="var(--color-notices)" radius={0} />
-				<ChartTooltip content={<ChartTooltipContent />} />
-				<Bar dataKey="todos" fill="var(--color-todos)" radius={0} />
-				<ChartTooltip content={<ChartTooltipContent />} />
-				<Bar dataKey="links" fill="var(--color-links)" radius={0} />
-				<ChartTooltip content={<ChartTooltipContent />} />
+				<Bar dataKey="projects" fill="var(--color-projects)" radius={4} />
+				<Bar dataKey="notices" fill="var(--color-notices)" radius={4} />
+				<Bar dataKey="todos" fill="var(--color-todos)" radius={4} />
+				<Bar dataKey="links" fill="var(--color-links)" radius={4} />
 			</BarChart>
 		</ChartContainer>
 	)
