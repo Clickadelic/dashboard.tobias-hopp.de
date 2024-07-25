a6f9e5e4371c00b4e8e4ca993ed74c1a9a2acbd7
Clickadelic <toby.hopp@gmail.com>
2024-07-25 16:48:08 +0200

    Cors Rückbau

--- app/api/public/v1/links/route.ts
+++ app/api/public/v1/links/route.ts
@@ -1,28 +1,20 @@
-import { db } from "@/lib/db"
-import { runMiddleware } from "@/lib/cors-middleware"
-import { NextApiRequest, NextApiResponse } from "next"
-import Cors from "cors"
+import { db } from "@/lib/db";
+import { runMiddleware } from "@/lib/cors-middleware";
+import { NextRequest, NextResponse } from "next/server";
+import Cors from "cors";
 
 // Initialisiere die cors-Middleware
 const cors = Cors({
 	methods: ["GET", "HEAD", "POST"], // Passen Sie die Methoden nach Bedarf an
 	origin: "https://dasboard.tobias-hopp.de" // Ersetzen Sie dies durch die spezifische Domain
-})
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	// Laufen Sie die CORS-Middleware
-	await runMiddleware(req, res, cors)
-
-	// Ihre API-Logik hier
-	res.json({ message: "Hello World" })
-}
+});
 
 export async function GET() {
-	const links = await db.link.findMany()
-	return new Response(JSON.stringify(links))
+	const links = await db.link.findMany();
+	return new Response(JSON.stringify(links));
 }
 
 export async function POST(request: Request) {
-	const { url, title } = await request.json()
-	console.log(url, title)
+	const { url, title } = await request.json();
+	console.log(url, title);
 }
