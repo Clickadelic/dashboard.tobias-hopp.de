c57faa788aa4da219e76568f838d17b519ff4c78
clickadelic <toby.hopp@gmail.com>
2024-07-25 14:24:53 +0200

    Cors test fix

--- app/api/public/v1/links/route.ts
+++ app/api/public/v1/links/route.ts
@@ -1,28 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from "next"
 import { db } from "@/lib/db"
-import { runMiddleware } from "@/lib/cors-middleware"
-import { NextApiRequest, NextApiResponse } from "next"
-import Cors from "cors"
+import { runMiddleware, cors } from "@/lib/cors-middleware"
 
-// Initialisiere die cors-Middleware
-const cors = Cors({
-	methods: ["GET", "HEAD", "POST"], // Passen Sie die Methoden nach Bedarf an
-	origin: "https://dasboard.tobias-hopp.de" // Ersetzen Sie dies durch die spezifische Domain
-})
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function GET(req: NextApiRequest, res: NextApiResponse) {
 	// Laufen Sie die CORS-Middleware
 	await runMiddleware(req, res, cors)
 
-	// Ihre API-Logik hier
-	res.json({ message: "Hello World" })
-}
-
-export async function GET() {
-	const links = await db.link.findMany()
-	return new Response(JSON.stringify(links))
-}
-
-export async function POST(request: Request) {
-	const { url, title } = await request.json()
-	console.log(url, title)
+	// Verarbeiten Sie die POST-Anfrage
+	try {
+		res.status(200).json({ message: "Data saved successfully" })
+	} catch (error) {
+		res.status(500).json({ error: "Internal Server Error" })
+	}
 }
--- lib/cors-middleware.ts
+++ lib/cors-middleware.ts
@@ -1,17 +1,10 @@
-// lib/cors-middleware.js
 import { NextApiRequest, NextApiResponse } from "next"
 import Cors from "cors"
 
-// Initialisieren Sie das cors-Middleware
-const cors = Cors({
-	methods: ["GET", "HEAD", "POST"], // Passen Sie die Methoden nach Bedarf an
-	origin: "*" // Erlaubt alle Ursprünge. Setzen Sie dies auf die spezifische Domain, wenn nötig
-})
-
+// Definiere die Signatur der Middleware-Funktion
 type MiddlewareFn = (req: NextApiRequest, res: NextApiResponse, next: (err?: any) => void) => void
 
-// Helper Methode zum Promisify der Middleware
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: MiddlewareFn) {
+export function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: MiddlewareFn) {
 	return new Promise<void>((resolve, reject) => {
 		fn(req, res, result => {
 			if (result instanceof Error) {
@@ -22,5 +15,8 @@ function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Middleware
 	})
 }
 
-export default cors
-export { runMiddleware }
+// Initialisiere die cors-Middleware
+export const cors = Cors({
+	methods: ["GET", "HEAD", "POST"], // Passen Sie die Methoden nach Bedarf an
+	origin: "https://dasboard.tobias-hopp.de" // Ersetzen Sie dies durch die spezifische Domain
+})
