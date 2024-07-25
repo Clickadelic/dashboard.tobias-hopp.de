c6282c0abd506a6ecc82a397d71f8153b794ad99
clickadelic <toby.hopp@gmail.com>
2024-07-25 14:47:11 +0200

    geht das??? :-D

--- app/api/public/v1/links/route.ts
+++ app/api/public/v1/links/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server"
 import { db } from "@/lib/db"
 
 export async function GET(req: NextRequest, res: NextResponse) {
-	const message = { message: "Hello World" }
+	const links = await db.link.findMany()
 
-	return NextResponse.json({ error: "Error capturing screenshot" }, { status: 500 })
+	return NextResponse.json(links, { status: 200 })
 }
