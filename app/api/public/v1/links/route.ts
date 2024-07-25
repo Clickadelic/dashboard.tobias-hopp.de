bbe50da28572fef6c067c77ca7ba12f8849a8962
clickadelic <toby.hopp@gmail.com>
2024-07-25 14:44:09 +0200

    asdasdasd

--- app/api/public/v1/links/route.ts
+++ app/api/public/v1/links/route.ts
@@ -1,11 +1,8 @@
 import { NextRequest, NextResponse } from "next/server"
 
-import { runMiddleware, cors } from "@/lib/cors-middleware"
-
 import { db } from "@/lib/db"
 
 export async function GET(req: NextRequest, res: NextResponse) {
-	await runMiddleware(req, res, cors)
 	const message = { message: "Hello World" }
 
 	return NextResponse.json({ error: "Error capturing screenshot" }, { status: 500 })
