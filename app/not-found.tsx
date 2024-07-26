756f3f2718a625a645c319e01fd6bac2322153ae
Clickadelic <toby.hopp@gmail.com>
2024-07-26 08:17:32 +0200

    Not found responsive test

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,9 @@
 import Image from "next/image";
 const Error404Page = () => {
 	return (
-		<div className="bg-white rounded-lg p-2 md:p-4 py-12 flex flex-col justify-center items-center">
-			<Image src="/images/svg/error-404.svg" className="mt-24 mb-12" width={300} height={300} alt="404" />
-			<h1 className="text-6xl text-slate-400 font-bold my-5">Fehler 404</h1>
+		<div className="flex flex-col items-center justify-center p-2 py-12 bg-white rounded-lg md:p-4">
+			<Image src="/images/svg/error-404.svg" layout="responsive" className="mt-24 mb-12" width={300} height={300} alt="404" />
+			<h1 className="my-5 text-6xl font-bold text-slate-400">Fehler 404</h1>
 			<h2 className="my-3 text-slate-700">Diese Seite wurde entführt.</h2>
 		</div>
 	);
