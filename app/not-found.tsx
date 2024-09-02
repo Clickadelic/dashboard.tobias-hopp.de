7e214886deab2e0a9711867fca60a6af5d220921
clickadelic <toby.hopp@gmail.com>
2024-09-02 18:05:51 +0200

    404 image no longer responsive

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,13 @@
-"use client";
-import Image from "next/image";
+"use client"
+import Image from "next/image"
 const Error404Page = () => {
 	return (
 		<div className="flex flex-col items-center justify-center p-2 py-12 bg-white rounded-lg md:p-4">
-			<Image src="/images/svg/error-404.svg" layout="responsive" className="mt-24 mb-12" width={300} height={300} alt="404" />
+			<Image src="/images/svg/error-404.svg" className="mt-24 mb-12" width={300} height={300} alt="404" />
 			<h1 className="my-5 text-6xl font-bold text-slate-400">Fehler 404</h1>
 			<h2 className="my-3 text-slate-700">Diese Seite wurde entführt.</h2>
 		</div>
-	);
-};
+	)
+}
 
-export default Error404Page;
+export default Error404Page
