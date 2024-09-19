15694ddfb8f4ae0672753f46804d96c2cd5e270e
clickadelic <toby.hopp@gmail.com>
2024-09-19 23:47:01 +0200

    Login Debug WIP

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async headers() {
-		return [
-			{
-				// matching all API routes
-				source: "/api/:path*",
-				headers: [
-					{ key: "Access-Control-Allow-Credentials", value: "true" },
-					{ key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-					{ key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-					{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-				]
-			},
-			{
-				// matching all static files
-				source: "/(.*)",
-				headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }]
-			}
-		];
-	},
+	// async headers() {
+	// 	return [
+	// 		{
+	// 			// matching all API routes
+	// 			source: "/api/:path*",
+	// 			headers: [
+	// 				{ key: "Access-Control-Allow-Credentials", value: "true" },
+	// 				{ key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+	// 				{ key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+	// 				{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+	// 			]
+	// 		},
+	// 		{
+	// 			// matching all static files
+	// 			source: "/(.*)",
+	// 			headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }]
+	// 		}
+	// 	];
+	// },
 	images: {
 		remotePatterns: [
 			{
@@ -35,6 +35,6 @@ const nextConfig = {
 			}
 		]
 	}
-};
+}
 
-export default nextConfig;
+export default nextConfig
