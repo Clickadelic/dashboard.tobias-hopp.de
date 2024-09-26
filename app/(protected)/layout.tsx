c68d9e7f7b7b62ded0f440c3e8350520ffc42af1
clickadelic <toby.hopp@gmail.com>
2024-09-26 17:21:40 +0200

    rückbau

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -6,16 +6,8 @@ interface ProtectedLayoutProps {
 }
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
-	const fetchPath = async () => {
-		const imagePath = await getUserBackground()
-		return imagePath
-	}
-
-	const myPath = fetchPath()
-
-	console.log("imagePath:", myPath)
 	return (
-		<div className="min-h-screen flex" style={{ backgroundImage: `url(${myPath})` }}>
+		<div className="min-h-screen flex">
 			<LayoutContext>{children}</LayoutContext>
 		</div>
 	)
