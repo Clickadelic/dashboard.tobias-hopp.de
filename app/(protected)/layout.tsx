aed0afe09ddd1a2d6bb3397c3dc63156eb6a787b
clickadelic <toby.hopp@gmail.com>
2024-09-26 17:52:22 +0200

    Background Image

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,12 +1,14 @@
 import LayoutContext from "./layout-context"
+import { getUserBackground } from "@/actions/upload"
 
 interface ProtectedLayoutProps {
 	children: React.ReactNode
 }
 
-const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
+const ProtectedLayout = async ({ children }: ProtectedLayoutProps) => {
+	const userBackground = await getUserBackground()
 	return (
-		<div className="min-h-screen bg-mantis-background flex">
+		<div className="min-h-screen bg-mantis-background flex" style={{ backgroundImage: `url(${userBackground})` || "none", backgroundSize: "cover" }}>
 			<LayoutContext>{children}</LayoutContext>
 		</div>
 	)
