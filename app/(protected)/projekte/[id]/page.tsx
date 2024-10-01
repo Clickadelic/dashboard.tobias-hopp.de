03263699d2fd9094d15dff9ecc033c7d7c20b65c
Clickadelic <toby.hopp@gmail.com>
2024-10-02 01:29:16 +0200

    delete project

--- app/(protected)/projekte/[id]/page.tsx
+++ app/(protected)/projekte/[id]/page.tsx
@@ -2,11 +2,26 @@ import { getProjectById } from "@/actions/project";
 import { FormProject } from "@/components/forms/form-project";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Link from "next/link";
+import { deleteProjectById } from "@/actions/project";
+import { toast } from "sonner";
 
 const ProjectByIdPage = async ({ params }: { params: { id: string } }) => {
 	const id = params.id;
 	const project = await getProjectById(id);
 
+	const onDelete = async () => {
+		try {
+			const result = await deleteProjectById(id);
+			if (result.error) {
+				toast.error(result.error);
+			} else if (result.success) {
+				toast.success(result.success);
+				window.location.href = "/projekte";
+			}
+		} catch (error) {
+			toast.error("Irgendwas ging schief - Ursache unbekannt");
+		}
+	};
 	return (
 		<div className="page-wrapper">
 			<h2 className="text-md flex justify-start font-bold text-slate-700 mb-5">Projektdetails</h2>
@@ -26,6 +41,14 @@ const ProjectByIdPage = async ({ params }: { params: { id: string } }) => {
 					</TabsContent>
 					<TabsContent value="edit">{project && <FormProject project={project} />}</TabsContent>
 				</Tabs>
+				<button
+					onClick={() => {
+						onDelete();
+					}}
+					className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+				>
+					Delete Projekt
+				</button>
 			</div>
 		</div>
 	);
