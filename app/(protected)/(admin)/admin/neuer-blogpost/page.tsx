4c3209e00c5cd84f3d843e094a1db7f8cc769938
Clickadelic <toby.hopp@gmail.com>
2024-11-26 01:21:56 +0100

    Test

--- app/(protected)/(admin)/admin/neuer-blogpost/page.tsx
+++ app/(protected)/(admin)/admin/neuer-blogpost/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NewBlogpostPage = () => {
-	return <div>Neuer Blogpost Page</div>;
+	return <div>Neuer Blogpost Page Test</div>;
 };
 
 export default NewBlogpostPage;
