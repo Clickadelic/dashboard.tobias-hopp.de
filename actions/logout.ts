9e56f107ef1f6ee9d4511dc2f3b736c51c36a7a5
Tobias Hopp <tobias.hopp@alzura.com>
2025-02-20 19:52:35 +0100

    RESET to 2ecf2665879d99ebfc06de186fefdda750c970e9

--- actions/logout.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { signOut } from "@/auth";
-
-export const logout = async () => {
-	// Server Stuff here before logout
-	await signOut({ redirect: true, redirectTo: "/auth/login" });
-};
