78ec62a8b60bac319a3428cb9a3b8e36fc205145
Tobias <Clickadelic@users.noreply.github.com>
2025-02-23 01:27:53 +0100

    context

---
+++ context/app-context.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { createContext, useState, ReactNode, Dispatch, SetStateAction, useContext } from "react"
+
+interface AppContextProps {
+	isSidebarOpen: boolean
+	setSidebarOpen: Dispatch<SetStateAction<boolean>>
+
+	isLinkDialogOpen: boolean
+	setLinkDialogOpen: Dispatch<SetStateAction<boolean>>
+
+	isProjectDialogOpen: boolean
+	setProjectDialogOpen: Dispatch<SetStateAction<boolean>>
+
+	isNoticeDialogOpen: boolean
+	setNoticeDialogOpen: Dispatch<SetStateAction<boolean>>
+
+	isTodoDialogOpen: boolean
+	setTodoDialogOpen: Dispatch<SetStateAction<boolean>>
+}
+
+const AppContext = createContext<AppContextProps | undefined>(undefined)
+
+interface AppProviderProps {
+	children: ReactNode
+}
+
+const AppProvider = ({ children }: AppProviderProps) => {
+	const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false)
+
+	const [isLinkDialogOpen, setLinkDialogOpen] = useState<boolean>(false)
+	const [isProjectDialogOpen, setProjectDialogOpen] = useState<boolean>(false)
+	const [isNoticeDialogOpen, setNoticeDialogOpen] = useState<boolean>(false)
+	const [isTodoDialogOpen, setTodoDialogOpen] = useState<boolean>(false)
+
+	return (
+		<AppContext.Provider
+			value={{
+				isSidebarOpen,
+				setSidebarOpen,
+				isLinkDialogOpen,
+				setLinkDialogOpen,
+				isProjectDialogOpen,
+				setProjectDialogOpen,
+				isNoticeDialogOpen,
+				setNoticeDialogOpen,
+				isTodoDialogOpen,
+				setTodoDialogOpen
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	)
+}
+
+const useAppContext = () => {
+	const context = useContext(AppContext)
+	if (context === undefined) {
+		throw new Error("useAppContext must be used within an AppProvider")
+	}
+	return context
+}
+
+export { AppProvider, useAppContext }
